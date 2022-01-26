a643a0e748435a0c5b8e7dc183d3303dcebf5d8a
stagas <gstagas@gmail.com>
2022-01-26 18:28:52 +0200

    fix: preserve trampoline stack trace using await

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,27 @@
 export const debounce = (inner: (...args: any[]) => any) => {
   let queued = false
   let params: any[] = []
+  let resolve: () => void
+  let reject: (error: unknown) => void
   function outer(this: any) {
     try {
       inner.apply(this, params)
+      resolve()
+    } catch (error) {
+      reject(error)
     } finally {
       queued = false
     }
   }
-  return function (...args: any[]) {
+  return async function (...args: any[]) {
     params = args
     if (queued) return
     queued = true
-    queueMicrotask(outer)
+    await new Promise<void>((_resolve, _reject) => {
+      resolve = _resolve
+      reject = _reject
+      queueMicrotask(outer)
+    })
   }
 }
 
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,9 +1,9 @@
-import { debounce } from '../'
+import { debounce } from '../src'
 
 // https://github.com/facebook/jest/issues/5620
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const process: any
-beforeAll(() => process.actual().removeAllListeners('uncaughtException'))
+beforeAll(() => process.actual().removeAllListeners('unhandledRejection'))
 
 describe('debounce', () => {
   it('simple', done => {
@@ -31,7 +31,7 @@ describe('debounce', () => {
     })
   })
 
-  it('when exception is thrown it resumes correctly', async () => {
+  it('on error it resumes correctly', async () => {
     let x = 0
 
     const fn = debounce((y: number) => {
@@ -46,9 +46,41 @@ describe('debounce', () => {
     fn()
 
     const error: Error = await new Promise(resolve =>
-      process.actual().once('uncaughtException', resolve)
+      process.actual().once('unhandledRejection', resolve)
     )
+    expect(error.stack).toContain('debounce-micro/test/')
+    expect(error.message).toContain('some error')
+    expect(x).toEqual(0)
 
+    fn(2)
+    fn(4)
+    expect(x).toEqual(0)
+
+    await new Promise<void>(resolve => queueMicrotask(resolve))
+
+    expect(x).toEqual(4)
+  })
+
+  it('awaiting can catch error and preserves stack trace', async () => {
+    let x = 0
+
+    const fn = debounce((y: number) => {
+      if (!y) {
+        throw new Error('some error')
+      } else {
+        x += y
+      }
+    })
+
+    let error: Error
+
+    try {
+      await fn()
+    } catch (_error) {
+      error = _error
+    }
+
+    expect(error.stack).toContain('debounce-micro/test/')
     expect(error.message).toContain('some error')
     expect(x).toEqual(0)
 
